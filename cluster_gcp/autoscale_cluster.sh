0a327cfe32a582e6b5ff01ca5437131814e8c438
Shu Yan <yanshu.usc@gmail.com>
2019-02-14 09:52:45 -0500

    change node type

--- cluster_gcp/autoscale_cluster.sh
+++ cluster_gcp/autoscale_cluster.sh
@@ -3,11 +3,11 @@
 gcloud beta container node-pools create user-pool \
   --cluster nycdsa-cloudlab \
   --preemptible \
-  --machine-type n1-standard-2 \
+  --machine-type n1-standard-4 \
   --num-nodes 0 \
   --enable-autoscaling \
   --min-nodes 0 \
-  --max-nodes 3 \
+  --max-nodes 4 \
   --zone us-east1-b \
   --node-labels hub.jupyter.org/node-purpose=user \
   --node-taints hub.jupyter.org_dedicated=user:NoSchedule
--- cluster_gcp/create_cluster.sh
+++ cluster_gcp/create_cluster.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 
 gcloud container clusters create \
-  --machine-type n1-standard-2 \
-  --num-nodes 2 \
+  --machine-type n1-standard-4 \
+  --num-nodes 1 \
+  --enable-autoscaling \
+  --min-nodes 0 \
+  --max-nodes 3 \
   --zone us-east1-b \
   --cluster-version latest \
   nycdsa-cloudlab
